d95a84659697f8b3ac1386b153c884b534cc8756
paolavasileva22 <p.vasileva2018@pmg-pernik.com>
2023-02-08 12:14:30 +0200

    WebApp

--- OpticaShopWebApp/OpticaShopWebApp/Data/ApplicationDbContext.cs
+++ OpticaShopWebApp/OpticaShopWebApp/Data/ApplicationDbContext.cs
@@ -1,17 +1,23 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using OpticaShopWebApp.Domain;
 using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace OpticaShopWebApp.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
             this.Database.EnsureCreated();
         }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Brand> Brands { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Order> Orders { get; set; }
+
     }
 }
